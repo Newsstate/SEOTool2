78f77728f8916bf473b2c67f9bbb56180d39f2df
Newsstate <newsstate6294@gmail.com>
2025-08-19 12:11:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ python -m playwright install chromium
 
 # (Optional) Show versions to the build log
 python -c "import sys; import playwright; print('Python', sys.version); print('Playwright', playwright.__version__)"
+
+git add build.sh
+git update-index --chmod=+x build.sh
+git commit -m "Use build.sh for Playwright + Chromium install"
